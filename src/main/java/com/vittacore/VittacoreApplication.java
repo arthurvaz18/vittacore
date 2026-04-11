c4ce04418bbe3adf28eeeefae6781ded9862eb18
arthurvaz18 <arthurvaz1805@gmail.com>
2026-04-10 22:25:05 -0300

    CHANGELOG [vittacore] - Passando arquivos importantes que nao tinham sido salvos

---
+++ src/main/java/com/vittacore/VittacoreApplication.java
@@ -0,0 +1,13 @@
+package com.vittacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VittacoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VittacoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vittacore/VittacoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vittacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VittacoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
